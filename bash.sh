5778340dd27604170ca1ca392a941daf4ec13d5e
babyair1 <sendysendy94@gmail.com>
2021-03-10 10:32:05 +0700

    add from wsl

--- bash.sh
+++ bash.sh
@@ -3,3 +3,4 @@
 echo "file ini dibuat menggunakan github webhook" > myfile.txt
 echo "'$BUILD_NUMBER' + '$USER'" >> myfile.txt
 echo "hallo ini baru yang dihasilkan oleh github trigger!" >> myfile.txt
+echo 'baris keempat ditulis dari wsl' >> mufile.txt
